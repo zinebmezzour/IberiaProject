a6527bfd77b01f5811b744c8e3e3152b7f49a345
Zineb Mezzour <zinebmezzour2610@gmail.com>
2019-03-15 17:45:19 +0100

    tst

--- app.py
+++ app.py
@@ -13,19 +13,21 @@ import dash_html_components as html
 import pandas as pd
 import psycopg2 as pg
 from dash.dependencies import Input, Output
+import pandas.io.sql as psql
+import dash_table_experiments as dt
 
 
+
+
+conn = pg.connect("postgres://bfkubnkgitlzqd:a0f9bca6a8136865f54a307c8cb18b6a36b541640e63f524d7a0938d59d4ff16@ec2-184-73-216-48.compute-1.amazonaws.com:5432/dcsrq4nf3tt2vq")
+
+df = pd.read_sql_query('select * from "App";', conn)
 
 
 
 app = dash.Dash(__name__)
 server = app.server
 
-conn = pg.connect("postgres://bfkubnkgitlzqd:a0f9bca6a8136865f54a307c8cb18b6a36b541640e63f524d7a0938d59d4ff16@ec2-184-73-216-48.compute-1.amazonaws.com:5432/dcsrq4nf3tt2vq")
-
-df = pd.read_sql_query('select * from "App";', conn)
-
-app = dash.Dash(__name__)
 
 app.layout = html.Div(children=[
         
