26064568846fba3ad8fe9a6268f0b440f7d8728a
Zineb Mezzour <zinebmezzour2610@gmail.com>
2019-03-29 14:55:06 +0100

    new app

--- app.py
+++ app.py
@@ -25,26 +25,69 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 conn = pg.connect("postgres://woneckmtfjjwnc:31b3c946995783dc7096969c88bc2f841fab2d73e0b2c159267ebc8f53d54255@ec2-75-101-133-29.compute-1.amazonaws.com:5432/d55rsv1snnhe39")
 
 dff=pd.read_sql_query('select * from "JanFeb";', conn)
-df = pd.read_sql_query('select "IncidentStatus","IncidentCode","DomainGroup", "Priority" from "JanFeb";', conn)
-df1 = pd.read_sql_query('select "CriticalStatus", "SupportDpt","SLA","ApplicationCode" from "App";', conn)
+c_df=pd.read_sql_query('select * from "critic";', conn)
+jf_df=pd.read_sql_query('select * from "janfebraised";', conn)
+servAvail_df=pd.read_sql_query('select * from "ServAvailJF";', conn)
+servRelial_df=pd.read_sql_query('select * from "ServReliabilityJF";', conn)
+
+
 
 #Add Features
 dff['year'] = pd.DatetimeIndex(dff['CreationDate']).year
 dff['month'] = pd.DatetimeIndex(dff['CreationDate']).month
 
-c_df=pd.read_sql_query('select * from "critic";', conn)
 c_df['year'] = pd.DatetimeIndex(c_df['CreationDate']).year
 c_df['month'] = pd.DatetimeIndex(c_df['CreationDate']).month
 
 
+jf_df['year'] = pd.DatetimeIndex(jf_df['CreationDate']).year
+jf_df['month'] = pd.DatetimeIndex(jf_df['CreationDate']).month
 
+
+critical_services_options= servAvail_df['Service'].unique()
 
 domain_options=dff["Domain"].unique()
 months_options=dff["month"].unique()
+service_options=dff["Service"].unique()
+
 
 
 c_domain_options=c_df["Domain"].unique()
 c_months_options=c_df["month"].unique()
+
+
+
+
+
+def generate_table(dataframe, max_rows=10):
+    '''Given dataframe, return template generated using Dash components
+    '''
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+        ]) for i in range(min(len(dataframe), max_rows))]
+    )
+
+def indicator(color, text, id_value):
+    return html.Div(
+        [
+            
+            html.P(
+                text,
+                className="twelve columns indicator_text"
+            ),
+            html.P(
+                id = id_value,
+                className="indicator_value"
+            ),
+        ],
+        className="four columns indicator",
+        
+    )
 
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets, url_base_pathname='/dashboard/')
@@ -77,6 +120,8 @@ html.Div([
 
     className="row header" 
     ), 
+
+    
  
 
 html.Div([
@@ -118,12 +163,6 @@ html.Div([
     style={"margin": "0%"},
     ),
 
-
-dash_table.DataTable(
-id='table',
-columns=[{"name": i, "id": i} for i in df.columns],
-data=df.to_dict("rows"),
-)
 ]
 )
 
@@ -143,29 +182,55 @@ def render_content(tab):
 
 
 general_layout = html.Div([
-    html.H2("Overview Incidences"),
-
+    
     html.Div([
-        dcc.Dropdown(
-        id='dropdown-1',
-        options=[{'label': i, 'value': i} for i in dff['year'].unique()],
-        value=2018
-        )
-    ]),
+        html.Div([ 
+        indicator('blue','my first indicator','indicator1'),
+        ]),
+
+        html.Div([ 
+        indicator('blue','my second indicator','indicator2'),
+        ]),
+        
+        html.Div([ 
+        indicator('blue','my third indicator','indicator3'),
+        ]),
 
 
-    dcc.Graph(id='graph1'),
+    ], className='row'),
+
+    html.Div([ 
+
+        html.Div([ 
+
+            html.Div([
+                dcc.Dropdown(
+                id='dropdown-1',
+                options=[{'label': i, 'value': i} for i in dff['year'].unique()],
+                value=2018
+                )
+            ]),
 
 
-    html.Div([
-        dcc.Dropdown(
-        id='dropdown-2',
-        options=[{'label': i, 'value': i} for i in dff['year'].unique()],
-        value=2018
-        )
-    ]),
+            dcc.Graph(id='graph1'),
+        ], className='six columns'),
 
-    dcc.Graph(id='graph2'),
+        html.Div([ 
+
+
+            html.Div([
+                dcc.Dropdown(
+                id='dropdown-2',
+                options=[{'label': i, 'value': i} for i in dff['year'].unique()],
+                value=2018
+                )
+            ]),
+
+            dcc.Graph(id='graph2'),
+
+        ], className='six columns'),
+
+    ], className='row'),
 
 
     html.Div([
@@ -188,8 +253,31 @@ general_layout = html.Div([
     dcc.Graph(id='graph3'),
 
 ],
-className= 'six columns'
+className= 'twelve columns'
 )
+
+
+   
+@app.callback(
+    Output("indicator1", "children"), [Input('dropdown-3', 'value')]
+)
+def middle_leads_indicator_callback(domain):
+    print(domain)
+    #df = pd.read_json(df, orient="split")
+    if domain == "All_Domains":
+        open_leads = len(
+        dff[
+            (dff["Priority"] == "Critical")
+        ].index
+    )
+    else:
+        open_leads = len(
+        dff[
+            (dff["Domain"] == domain)
+        ].index
+    )
+    
+    return open_leads
 
 
 @app.callback(
@@ -269,6 +357,7 @@ def graph_2(input):
         )}
 
 
+
 @app.callback(
     Output('graph1', 'figure'),
     [Input('dropdown-1', 'value') 
@@ -317,18 +406,14 @@ def graph_maker1(input1):
     }
 
 
-
-#df1 = pd.read_sql_query('select "CriticalStatus", "SupportDpt","SLA","ApplicationCode" from "App";', conn)
-
 #PERFORMANCE LAYOUT
 performance_layout = html.Div([
-        html.H2("Overview App"),
+        html.H2("Performance"),
 
             html.Div([
     
-    
                 html.Div([ 
-                    html.H3('Graph1'),
+                    
                     
                     html.Div([
                         dcc.Dropdown(
@@ -336,63 +421,165 @@ performance_layout = html.Div([
                         options=[{'label': i, 'value': i} for i in dff['year'].unique()],
                         value=2018
                         )
-                    ]),
-
-                    dcc.Graph(id='graph11'),
-                        ],
-                    className='six columns'),
+                    ], className='twelve columns'),
 
 
-                html.Div([
-    
-                        html.H3('Graph2'), 
+                html.Div([ 
 
+                    html.Div([
+                        html.H4('Graph1'),
+                        dcc.Graph(id='graph11')
+                    ], className='six columns'),
 
-                        html.Div([
-                        dcc.Dropdown(
-                        id='dropdown-22',
-                        options=[{'label': i, 'value': i} for i in dff['year'].unique()],
-                        value=2018
-                        )
-                            ]),
-
+                    html.Div([
+                        html.H4('Graph2'),
                         dcc.Graph(id='graph22'),
-                            ],
-                        className="six columns"
-                            ),
-                    
+                        
+                    ], className="six columns")
+
+                ],className='row')
+
+
+                        ],
+                    className='twelve columns'),    
+       
             ],
             className="row"),
 
 
+
+            html.Div([ 
+
+
             html.Div([
-                html.H3('Graph3'),
-
-                html.Div([
-                    dcc.Dropdown(
-                    id='dropdown-33',
-                    options=[{'label': i, 'value': i} for i in c_domain_options],
-                    value='AIRPORTS'
+                html.H4('Availability of Critical Services per Month'),
+           dcc.Graph(
+                    figure=go.Figure(
+                    data=[
+                         go.Bar(
+                            x= servAvail_df['Service'].unique().tolist(),
+                            y= servAvail_df['January'].tolist(),
+                            name='January',
+                            marker=go.bar.Marker(
+                            color='rgb(55, 83, 109)'
+                                    )
+                     ),
+                        go.Bar(
+                            x= servAvail_df['Service'].unique().tolist(),
+                            y= servAvail_df['February'].tolist(),
+                            name='February',
+                            marker=go.bar.Marker(
+                                color='rgb(26, 118, 255)'
+                                )
+                            )
+                            ],
+                            layout=go.Layout(
+                                
+                                showlegend=True,
+                                yaxis={'title': '% Availability'},
+                                legend=go.layout.Legend(
+                                    x=0,
+                                    y=1.0
+                                ),
+                                margin=go.layout.Margin(l=60, r=40, t=40, b=100)
+                            )
+                        ),
+                        style={'height': 500},
+                        id='availability_graph'
                     )
-                ]),
+               ],
+            className='six columns'),
 
-                html.Div([
-                    dcc.Dropdown(
-                    id='dropdown-44',
-                    options=[{'label': i, 'value': i} for i in c_months_options],
-                    value=1
-                    )
-                ]),
-
-
-                dcc.Graph(id='graph33'),
             
-            ],
-            className='row'),
+
+            html.Div([
+                html.H4('Reliability of Critical Services per Month'),
+
+                dcc.Graph(
+                    figure=go.Figure(
+                    data=[
+                         go.Bar(
+                            x= servRelial_df['Service'].unique().tolist(),
+                            y= servRelial_df['January'].tolist(),
+                            name='January',
+                            marker=go.bar.Marker(
+                            color='rgb(55, 83, 109)'
+                                    )
+                     ),
+                        go.Bar(
+                            x= servRelial_df['Service'].unique().tolist(),
+                            y= servRelial_df['February'].tolist(),
+                            name='February',
+                            marker=go.bar.Marker(
+                                color='rgb(26, 118, 255)'
+                                )
+                            )
+                            ],
+                            layout=go.Layout(
+                                
+                                showlegend=True,
+                                yaxis={'title': 'Hours between Failures'},
+                                legend=go.layout.Legend(
+                                    x=0,
+                                    y=1.0
+                                ),
+                                margin=go.layout.Margin(l=60, r=100, t=0, b=100)
+                            )
+                        ),
+                        style={'height': 500},
+                        id='reliability_graph'
+                    )
+               ],
+            className='six columns'),
+
+
+
+            ], className='twelve columns')
 
 ],
 
 )
+
+@app.callback(
+    Output('graph44', 'figure'),
+    [Input('dropdown-55', 'value') 
+    ])
+
+
+def graph_44():
+    print(input)
+
+    filtered_df = servAvail_df[input].tolist()
+    
+    #filtered_df1 = servAvail_df['February'].tolist()
+
+    critical_services_options= servAvail_df['Service'].unique().tolist()
+
+ #   availability=[]
+
+ #   for i in critical_services_options :
+ #       p=filtered_df[i].unique()
+ #       p=p.tolist()
+  #      p=p[0]
+  #      availability.append(p)
+
+       
+    return ({
+        'data': [{'x':critical_services_options, 'y':filtered_df,'type':'scatter', 'name':input}
+    
+        ],
+        'layout': go.Layout(
+            xaxis={'type':'category', 'title': 'Services'},
+            yaxis={'title': '% Availability'},
+            barmode="group",
+            legend={'x': 0, 'y': 1},
+            hovermode='closest',
+            title="Availability for Critical Services in {}".format(input)
+            )
+        }
+        )
+
+
 
 
 @app.callback(
@@ -439,7 +626,7 @@ def graph_33(input,other):
 
 @app.callback(
     Output('graph22', 'figure'),
-    [Input('dropdown-22', 'value') 
+    [Input('dropdown-11', 'value') 
     ])
 
 def graph_22(input):
@@ -517,8 +704,7 @@ def graph_maker11(input1):
         )
     }
     
-    
-    
+ 
     
     
     
